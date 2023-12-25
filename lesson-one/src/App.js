424bd4178131699a892c3bcfa2d9fbbb7a7368e6
kadiiravsaar <kadiiravsar@gmail.com>
2023-12-25 17:53:50 +0300

    ilk commit

---
+++ lesson-one/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+export default function App() {
+  return (
+  //  <div>
+  //     <p>Hello</p>
+  //     <div>
+  //       <p>Kadir</p>
+  //     </div>
+  //  </div>
+
+  
+  <>
+    
+   
+    <div> 
+     <p>Kadir</p>
+    </div>
+      
+    <div>
+     <p>Kadir2</p>
+    </div>
+  
+  
+  
+  
+  </>
+  );
+}
+
