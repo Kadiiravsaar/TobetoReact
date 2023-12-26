e594d010d858cfac3152c2b66b2875bebbcd9d72
kadiiravsaar <kadiiravsar@gmail.com>
2023-12-27 00:09:55 +0300

    .

---
+++ Odevler/camp-project/src/Services/productService.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export default class ProductService {
+  getProducts() {
+    // return axios.get("ApiUrl");
+    //return axios.get("https://localhost:8080/api/products/getall");
+    //return axios.get("https://jsonplaceholder.typicode.com/posts");
+    return axios.get("https://dummyjson.com/products");
+  }
+}
